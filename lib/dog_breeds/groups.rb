d356e28b87651ea2c3dc2e06e5e5b0fc79b8efee
bethurban <beth.urban@gmail.com>
2018-02-16 18:52:48 -0500

    Created variables for group names in the Groups class.

--- lib/dog_breeds/groups.rb
+++ lib/dog_breeds/groups.rb
@@ -2,16 +2,27 @@ class DogBreeds::Groups
   def initialize
     #Will scrape scrape group names
     puts "These are the American Kennel Club's groups of dog breeds:"
+    url = "http://www.akc.org/dog-breeds/"
+    group_1 = "Sporting group"
+    group_2 = "Hound group"
+    group_3 = "Working group"
+    group_4 = "Terrier group"
+    group_5 = "Toy group"
+    group_6 = "Non-sporting group"
+    group_7 = "Herding group"
+    group_8 = "Miscellaneous class"
+    group_9 = "Foundation Stock Service"
+
     puts <<~DOC
-      1. Sporting group
-      2. Hound group
-      3. Working group
-      4. Terrier group
-      5. Toy group
-      6. Non-sporting group
-      7. Herding group
-      8. Miscellaneous class
-      9. Foundation Stock Service
+    1. #{group_1}
+    2. #{group_2}
+    3. #{group_3}
+    4. #{group_4}
+    5. #{group_5}
+    6. #{group_6}
+    7. #{group_7}
+    8. #{group_8}
+    9. #{group_9}
     DOC
   end
 end
