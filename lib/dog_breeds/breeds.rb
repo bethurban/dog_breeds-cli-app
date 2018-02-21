5bb52bc22cbaaaec49073224193c4c84dc245432
bethurban <beth.urban@gmail.com>
2018-02-21 18:00:01 -0500

    Built out #self.scrape_sporting method to correctly scrape and put out numbered list of breeds

--- lib/dog_breeds/breeds.rb
+++ lib/dog_breeds/breeds.rb
@@ -1,61 +1,68 @@
 class DogBreeds::Breeds
 
   def initialize
-    self.scrape_breeds
     puts "Which group of breeds would you like to see?"
     input = gets.strip
     case input
     when "1"
-      scrape_sporting
+      self.scrape_sporting
     when "2"
-      scrape_hound
+      self.scrape_hound
     when "3"
-      scrape_working
+      self.scrape_working
     when "4"
-      scrape_terrier
+      self.scrape_terrier
     when "5"
-      scrape_toy
+      self.scrape_toy
     when "6"
-      scrape_non_sporting
+      self.scrape_non_sporting
     when "7"
-      scrape_herding
+      self.scrape_herding
     when "8"
-      scrape_misc
+      self.scrape_misc
     when "9"
-      scrape_fss
+      self.scrape_fss
     else
     puts "Invalid entry. Please enter the number of the group you'd like to see."
     #Need to cycle back through and list groups and allow choice
     end
   end
 
-  def scrape_sporting
+  def self.scrape_sporting
     doc = Nokogiri::HTML(open("http://www.akc.org/dog-breeds/groups/sporting/"))
     binding.pry
+    breeds = []
+    dogs = doc.css('.event-contain .scale-contents h2')
+    dogs.each do |dog|
+      breeds << dog.css('a[href]').text
+    end
+    breeds.each_with_index do |breed, number|
+      puts "#{number + 1}. #{breed}"
+    end
   end
 
-  def scrape_hound
+  def self.scrape_hound
   end
 
-  def scrape_working
+  def self.scrape_working
   end
 
-  def scrape_terrier
+  def self.scrape_terrier
   end
 
-  def scrape_toy
+  def self.scrape_toy
   end
 
-  def scrape_non_sporting
+  def self.scrape_non_sporting
   end
 
-  def scrape_herding
+  def self.scrape_herding
   end
 
-  def scrape_misc
+  def self.scrape_misc
   end
 
-  def scrape_fss
+  def self.scrape_fss
   end
 
 end
